4c73dd3e752b8e8849b96088eded583997044e26
boogie <boogiepop@gmx.com>
2023-03-20 00:10:09 +0100

    rk_reg32: fix write bug

--- libmmm/devices/rockchip/__init__.py
+++ libmmm/devices/rockchip/__init__.py
@@ -6,7 +6,8 @@ class RK_Reg32_16bitMasked(Reg32):
         oldval = self.readraw()
         for start, size, datapoint in self.iterdatapoints():
             if datapoint.name == name:
-                # set bot the the sart and start + 16 with write mask set to true
+                value = self._checkvalidity(datapoint, value)
+                # set both the the sart and start + 16 with write mask set to true
                 newval = struct.unpack("%sI" % self.endian, self._setbit(oldval, start, size, value))[0]
-                super(Reg32, self).write(self._setbit(newval, 16 + start, 2 ** size, value))
+                super(Reg32, self).write(self._setbit(newval, 16 + start, size, 2 ** (size - 1)))
                 return True
